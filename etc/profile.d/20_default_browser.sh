4b9379b63247dd9cc2014174b26a117bcdd3c84b
Patrick Schleizer <adrelanos@whonix.org>
2021-08-04 12:43:20 -0400

    lintian FHS

--- etc/profile.d/20_default_browser.sh
+++ etc/profile.d/20_default_browser.sh
@@ -3,6 +3,6 @@
 ## Copyright (C) 2012 - 2021 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-if [ -x /usr/lib/tb-default-browser/tb_default_browser.sh ]; then
-   . /usr/lib/tb-default-browser/tb_default_browser.sh
+if [ -x /usr/libexec/tb-default-browser/tb_default_browser.sh ]; then
+   . /usr/libexec/tb-default-browser/tb_default_browser.sh
 fi

96d0b192ffe1bfeb9ca2651ecbf1a0762d1fad33
Patrick Schleizer <adrelanos@whonix.org>
2023-10-12 12:08:54 -0400

    refactoring

--- usr/libexec/tb-default-browser/tb_default_browser.sh
+++ usr/libexec/tb-default-browser/tb_default_browser.sh
@@ -3,6 +3,11 @@
 ## Copyright (C) 2012 - 2023 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
+if [ "$XDG_SESSION_TYPE" = "tty" ]; then
+   ## Skip this in tty.
+   return 0
+fi
+
 ## See also:
 ## The Default Browser on Linux Debacle
 ## http://blog.codef00.com/2011/02/18/the-default-browser-on-linux-debacle/
